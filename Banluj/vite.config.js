17bb359a73eaabf7f7b92bf46c123b38f5243ec5
gasa_Angel <a20624646@gmail.com>
2025-08-14 10:33:38 -0600

    first

--- Banluj/vite.config.js
+++ Banluj/vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Esta es la carpeta que Vite creará automáticamente
+    emptyOutDir: true,
+    manifest: true // Genera un manifest.json para mejor cache
+  },
+  server: {
+    historyApiFallback: true // Importante para SPA
+  }
 })
